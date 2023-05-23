49ff11a7166b5cd13145cb7f984743e505b927ca
nanigen <otoo.danieru@gmail.com>
2023-05-23 17:12:59 +0900

    Initial commit

---
+++ manifest.scm
@@ -0,0 +1,8 @@
+(specifications->manifest
+  (list "python"
+        "python-ipython"
+        "python-ipykernel"
+        "jupyter"
+        "guix-jupyter"
+        "python-beautifulsoup4"
+        "python-pandas"))
