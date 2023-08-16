c869c2f5f9341f0fcd9d5aa27b09ae3b95adc62b
nanigen <otoo.danieru@gmail.com>
2023-08-16 13:22:22 +0900

    Further readability refactoring on scraping funcs

--- tests/test_suumo.py
+++ tests/test_suumo.py
@@ -1,9 +1,10 @@
 from suumo import *
 
-def test_house_collector():
+def test_rental_listing_collection():
+    """Test if rental listings are collected correctly."""
     expected_houses = 30
     # houses = house_collector(1, 2) # TODO use randint() = x-1
     # Check if the houses are collected correctly
-    assert sum(1 for _ in house_collector(1, 2)) == 30
+    assert sum(1 for _ in collect_rental_listings(1, 2)) == 30
     # for house, expected_house in zip(houses, expected_houses):
     #     assert house.text.strip() == expected_house
