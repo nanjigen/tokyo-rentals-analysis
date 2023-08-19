6bb6c391a6c85360310f381d001e8a6fddef1d2c
nanigen <otoo.danieru@gmail.com>
2023-08-19 16:27:08 +0900

    Add search_url as parameter to functions

--- tests/test_suumo.py
+++ tests/test_suumo.py
@@ -3,4 +3,4 @@ from suumo.suumo import *
 def test_number_of_rental_listings():
     """Test if the expected number of rental listings are collected per page."""
     expected_listings = 30
-    assert sum(1 for _ in collect_rental_listings(1, 2)) == expected_listings
+    assert sum(1 for _ in collect_rental_listings(search_url, 1, 2)) == expected_listings
