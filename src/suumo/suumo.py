178f7c5a63f6746d7145337b18832d57bb225fe0
nanigen <otoo.danieru@gmail.com>
2023-08-21 11:20:11 +0900

    Return integer

--- src/suumo/suumo.py
+++ src/suumo/suumo.py
@@ -80,7 +80,7 @@ def fetch_results_per_page(search_url):
     soup = BeautifulSoup(response.content, "html.parser")
     select_element = soup.find('select', id='js-tabmenu2-pcChange')
     selected_option = select_element.find('option', selected=True)
-    selected_value = selected_option['value']
+    selected_value = int(selected_option['value'])
     return selected_value
 
     page_nr = [int(s) for s in page_nr.split() if s.isdigit()]
