c4bce65205a1bbcef56f0926de6786984ab64c66
nanigen <otoo.danieru@gmail.com>
2023-09-04 12:41:37 +0900

    Refactor towards spider class and scrapy functions

--- tests/test_suumo.py
+++ tests/test_suumo.py
@@ -1,13 +1,9 @@
 from suumo.suumo import *
 
-@pytest.fixture
-def search_url():
-    return "http://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&sc=13104&sc=13105&sc=13113&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2="
-
-def test_search_results(search_url):
+def test_search_results(response):
     """Test the number of returned search results against results per page * page count"""
-    expected_result_count = fetch_results_total_hits(search_url)
-    calculated_result_count = fetch_results_per_page(search_url) * fetch_total_pages_count(search_url)
+    expected_result_count = SuumoSpider().get_total_hits(response)
+    calculated_result_count = SuumoSpider().get_results_per_page(response) * SuumoSpider().get_total_pages(response)
     assert expected_result_count == calculated_result_count, \
         f"Expected {expected_result_count} results, but got {calculated_result_count} caculated results."
 
